1b1a67e0cd7f3ef9f77bf95e788d0dab19a631f7
Nicole Muldoon <n.muldoon.21@abdn.ac.uk>
2022-12-17 00:36:44 +0000

    Revamped entire website, added about.html and done majority of code for JS

--- ca4/index.js
+++ ca4/index.js
@@ -1,44 +1,59 @@
 const path = require('path')
 const http = require('http');
-const express = require("express");
-const socket = require("socket.io");
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('messages');
+const { userJoin, getCurrentUser, userLeave } = require('users'); 
 
 // App setup
-const PORT = 5000;
+
 const app = express();
-const server = app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
+const server =  http.createServer(app);
+const io = socketio(server);
 
-});
-
-// Static files
+// Static folders
 app.use(express.static(path.join(__dirname, 'public')));
+const adName = 'Administator';
 
-// Socket setup
-const io = socket(server);
+// When a client joins 
+io.on('connection', function (socket) {
 
-//we use a set to store users, sets objects are for unique values of any type
-const activeUsers = new Set();
+  socket.on('joinChat', ({ username }) => {
+     const user = userJoin(socket.id, username);
 
-io.on("connection", function (socket) {
-  console.log("Made socket connection");
+  
+  //Broadcast to user when they join chat
+  socket.emit('message', formatMessage(adName, 'Welcome to the chat!'));
 
-  socket.on("new user", function (data) {
-    socket.userId = data;
-    activeUsers.add(data);
-    //... is the the spread operator, adds to the set while retaining what was in there already
-    io.emit("new user", [...activeUsers]);
-  });
+  // Broadcast to chat when a new user joins
+  socket.broadcast.emit('message', formatMessage(adName, `${user.username} has joined the chat!`));
 
-  socket.on("disconnect", function () {
-      activeUsers.delete(socket.userId);
-      io.emit("user disconnected", socket.userId);
-    });
+});
 
-    socket.on("chat message", function (data) {
-      io.emit("chat message", data);
+  // Listen for chat message
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
+    io.emit('message', formatMessage(user.username, msg));
+  })
+
+  // When a client disconnects
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if(user) {
+      io.emit('message', formatMessage(adName, `${user.username} has left the chat!`));
+
+  }
+    
   });
 
 });
+
+const PORT = 5000;
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+
+
+
 
 
--- ca4/public/js/client.js
+++ ca4/public/js/client.js
@@ -1,116 +1,49 @@
-//required for front end communication between client and server
+const chatBox = document.getElementById('chatBox')
+const chatMessages = document.querySelector('.messages');
+
+// Get username
+
+const {username} = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+});
 
 const socket = io();
 
-const inboxPeople = document.querySelector(".inbox__people");
+// Enter chatroom
+socket.emit('joinChat', { username });
 
+// Server message
+socket.on('message', message => {
+  console.log(message);
+  outputMessage(message);
 
-let userName = "";
-let id;
-const newUserConnected = function (data) {
-    
+  // Scrolls chat down when a message is received
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+})
 
-    //give the user a random unique id
-    id = Math.floor(Math.random() * 1000000);
-    userName = 'user-' +id;
-    //console.log(typeof(userName));   
-    
-
-    //emit an event with the user id
-    socket.emit("new user", userName);
-    //call
-    addToUsersBox(userName);
-};
-
-const addToUsersBox = function (userName) {
-    //This if statement checks whether an element of the user-userlist
-    //exists and then inverts the result of the expression in the condition
-    //to true, while also casting from an object to boolean
-    if (!!document.querySelector(`.${userName}-userlist`)) {
-        return;
-    
-    }
-    
-    //setup the divs for displaying the connected users
-    //id is set to a string including the username
-    const userBox = `
-    <div class="chat_id ${userName}-userlist">
-      <h5>${userName}</h5>
-    </div>
-  `;
-    //set the inboxPeople div with the value of userbox
-    inboxPeople.innerHTML += userBox;
-};
-
-//call 
-newUserConnected();
-
-//when a new user event is detected
-socket.on("new user", function (data) {
-  data.map(function (user) {
-          return addToUsersBox(user);
-      });
-});
-
-//when a user leaves
-socket.on("user disconnected", function (userName) {
-  document.querySelector(`.${userName}-userlist`).remove();
-});
-
-
-const inputField = document.querySelector(".message_form__input");
-const messageForm = document.querySelector(".message_form");
-const messageBox = document.querySelector(".messages__history");
-
-const addNewMessage = ({ user, message }) => {
-  const time = new Date();
-  const formattedTime = time.toLocaleString("en-US", { hour: "numeric", minute: "numeric" });
-
-  const receivedMsg = `
-  <div class="incoming__message">
-    <div class="received__message">
-      <p>${message}</p>
-      <div class="message__info">
-        <span class="message__author">${user}</span>
-        <span class="time_date">${formattedTime}</span>
-      </div>
-    </div>
-  </div>`;
-
-  const myMsg = `
-  <div class="outgoing__message">
-    <div class="sent__message">
-      <p>${message}</p>
-      <div class="message__info">
-        <span class="time_date">${formattedTime}</span>
-      </div>
-    </div>
-  </div>`;
-
-  //is the message sent or received
-  messageBox.innerHTML += user === userName ? myMsg : receivedMsg;
-};
-
-messageForm.addEventListener("submit", (e) => {
+// Submit message
+chatBox.addEventListener('submit', (e) => {
   e.preventDefault();
-  if (!inputField.value) {
-    return;
-  }
+  
+  // Aquire text of message
+  const msg = e.target.elements.msg.value;
 
-  socket.emit("chat message", {
-    message: inputField.value,
-    nick: userName,
-  });
+  // Send message to the server
+  socket.emit('chatMessage', msg);
 
-  inputField.value = "";
-});
+  //Clear input form
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+})
 
-socket.on("chat message", function (data) {
-  addNewMessage({ user: data.nick, message: data.message });
-});
-
-
-
+// Output message to document
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('msg');
+  div.innerHTML = `<p class="name">${message.username} <span>${message.time}</span></p>
+  <p class="reply">${message.text}</p>`;
+  document.querySelector('.messages').appendChild(div);
+}
 
 
 
